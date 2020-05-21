5ed7606f56a576dac741de2062342b82337f0ed6
yose kornelius <yose.kornelius@ti.ukdw.ac.id>
2020-05-22 00:35:30 +0700

    ADD card view,recycler view tips manfaat (80%)

--- app/src/main/java/com/example/easyhealthy/adapter/ItemGroupHomeAdapter.java
+++ app/src/main/java/com/example/easyhealthy/adapter/ItemGroupHomeAdapter.java
@@ -6,43 +6,63 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.easyhealthy.R;
+import com.example.easyhealthy.ui.home.Model.ItemData;
+import com.example.easyhealthy.ui.home.Model.ItemGroup;
+
+import java.util.List;
 
 public class ItemGroupHomeAdapter extends RecyclerView.Adapter<ItemGroupHomeAdapter.MyviewHolder> {
 
     private Context context;
-    //private List<ItemGroup> datalist;
+    private List<ItemGroup> datalist;
 
-//    public ItemGroupHomeAdapter(Context context, List<ItemGroup> datalist) {
-//        this.context = context;
-//        this.datalist = datalist;
-//    }
+    public ItemGroupHomeAdapter(Context context, List<ItemGroup> datalist) {
+        this.context = context;
+        this.datalist = datalist;
+    }
 
     @NonNull
     @Override
     public MyviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemview = LayoutInflater.from( context ).inflate( R.layout.layout_group_home,parent,false );
-        return null;
+        return new MyviewHolder( itemview );
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyviewHolder holder, int position) {
+        holder.item_title.setText( datalist.get( position ).getHeaderTitle() );
+
+        List<ItemData>  itemData = datalist.get( position ).getListResep();
+
+        ItemHomeAdapter itemListAdapter = new ItemHomeAdapter( context,itemData );
+        holder.recyclerView_item_list.setHasFixedSize( true );
+        holder.recyclerView_item_list.setLayoutManager( new LinearLayoutManager( context,LinearLayoutManager.HORIZONTAL,false ) );
+        holder.recyclerView_item_list.setAdapter( itemListAdapter );
+
+        holder.recyclerView_item_list.setNestedScrollingEnabled( false );
+
+
+        //button more
+        holder.btn_more.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( context,"MORE", Toast.LENGTH_SHORT ).show();
+            }
+        } );
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return (datalist != null ? datalist.size() : 0);
     }
-
-//    @Override
-//    public int getItemCount() {
-//        return (datalist != null ? datalist.size() : 0);
-//    }
 
     public class MyviewHolder extends RecyclerView.ViewHolder {
         TextView item_title;
--- app/src/main/java/com/example/easyhealthy/interfaces/FirebaseLoadListenerInterface.java
+++ app/src/main/java/com/example/easyhealthy/interfaces/FirebaseLoadListenerInterface.java
@@ -1,8 +1,11 @@
 package com.example.easyhealthy.interfaces;
 
 
+import com.example.easyhealthy.ui.home.Model.ItemGroup;
+
+import java.util.List;
 
 public interface FirebaseLoadListenerInterface {
-    //void onFirebaseLoadSucces(List<ItemGroup> itemGroupList);
+    void onFirebaseLoadSucces(List<ItemGroup> itemGroupList);
     void onFirebaseLoadFailed(String message);
 }
